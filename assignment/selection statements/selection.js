515213672cf3ec71cd50ba9ebfb86b47b6581b99
Rishitha219 <85661212+Rishitha219@users.noreply.github.com>
2021-06-10 18:07:56 +0530

    Add files via upload

---
+++ assignment/selection statements/selection.js
@@ -0,0 +1,10 @@
+var course ='java';
+if(course == 'java'){
+  console.log('java course selected')
+}
+else if(course == 'dotnet'){
+console.log('dotnet course selected')
+}
+else{
+  console.log('angular course selected')
+}
